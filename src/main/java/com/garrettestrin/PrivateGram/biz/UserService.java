f2818e5eefde084753ed4b37b7d13663ccc84eea
Garrett Estrin <garrett.estrin@gmail.com>
2019-07-20 19:27:55 -0700

    Added Name, Email and Password validation

--- src/main/java/com/garrettestrin/PrivateGram/biz/UserService.java
+++ src/main/java/com/garrettestrin/PrivateGram/biz/UserService.java
@@ -5,6 +5,8 @@ import com.garrettestrin.PrivateGram.api.ApiObjects.User;
 import com.garrettestrin.PrivateGram.app.Auth.Auth;
 import com.garrettestrin.PrivateGram.app.PrivateGramConfiguration;
 import com.garrettestrin.PrivateGram.data.UserDao;
+import com.garrettestrin.PrivateGram.biz.BizObjects.ValidatedUserInformation;
+
 import io.jsonwebtoken.Claims;
 
 public class UserService {
@@ -41,8 +43,8 @@ public class UserService {
     public Message loginUser(String email, String password) {
         boolean isUserVefified = verifyPassword(email, password);
         if(isUserVefified)
-            return new Message("User Verification", isUserVefified, 200, auth.createJWT(email, "Garrett", "user validated", -1));
-        return new Message("User Verification", isUserVefified, 200, null);
+            return new Message("User Login", isUserVefified, 200, auth.createJWT(email, "Garrett", "user validated", -1));
+        return new Message("User Login", isUserVefified, 200, null);
     }
 
     // TODO: JAVADOC
@@ -52,6 +54,9 @@ public class UserService {
         try {
             Long userId = getUserIdByEmail(email);
             if(userId != null) return new Message("User already exists", false, 200, null);
+            ValidatedUserInformation validUserInformation = validateUserInformation(email, first_name, last_name, password);
+            if(!validUserInformation.email || !validUserInformation.first_name || !validUserInformation.last_name || !validUserInformation.password)
+                return new Message("Invalid information", false, 200, validUserInformation.toJSON());
             // hash password string
             String hashedPassword = auth.hashPassword(password);
             boolean userCreated = userDao.registerUser(email, first_name, last_name, hashedPassword);
@@ -74,7 +79,11 @@ public class UserService {
     // TODO: JAVADOC
     // TODO: Add Test
     public Message verifyToken(String token, String auth) {
-        if(!auth.equals(AUTH_TOKEN)) {
+        try {
+            if(!auth.equals(AUTH_TOKEN)) {
+                return unauthorized();
+            }
+        } catch (Exception e) {
             return unauthorized();
         }
         Claims claims = Auth.verifyJWT(token);
@@ -83,6 +92,22 @@ public class UserService {
             isVerified = !claims.getId().isEmpty();
         }
         return new Message("Token Verification", isVerified, 200, null);
+    }
+
+    public ValidatedUserInformation validateUserInformation(String email, String first_name, String last_name, String password) {
+        return new ValidatedUserInformation(validateEmail(email), validateName(first_name), validateName(last_name), validatePassword(password));
+    }
+
+    public boolean validateName(String name) {
+        return name.matches( "[a-zA-Z]+([ '-][a-zA-Z]+)*" );
+    }
+
+    public boolean validateEmail(String email) {
+        return email.matches("^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$");
+    }
+
+    public boolean validatePassword(String password) {
+        return password.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,16}$");
     }
 
     public Message unauthorized() {

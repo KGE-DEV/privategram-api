fcf44ce763950c4f3fcc761f2cd714bffc0352ac
Garrett Estrin <garrett.estrin@gmail.com>
2020-03-18 19:15:24 -0700

    Get all comments endpoint complete

--- src/main/java/com/garrettestrin/PrivateGram/biz/CommentService.java
+++ src/main/java/com/garrettestrin/PrivateGram/biz/CommentService.java
@@ -2,6 +2,9 @@ package com.garrettestrin.PrivateGram.biz;
 
 import com.garrettestrin.PrivateGram.api.ApiObjects.CommentResponse;
 import com.garrettestrin.PrivateGram.data.CommentDao;
+import com.garrettestrin.PrivateGram.data.DataObjects.Comment;
+
+import java.util.List;
 
 public class CommentService {
 
@@ -15,12 +18,13 @@ public class CommentService {
   public CommentResponse postComment(String comment, int postId, String userId) {
     boolean wasCommentPosted = commentDao.postComment(postId, comment, userId);
     if(wasCommentPosted) {
-      return new CommentResponse(true, null);
+      return new CommentResponse(true, null, null);
     }
-    return new CommentResponse(false, null);
+    return new CommentResponse(false, null, null);
   }
 
   public CommentResponse getAllComments(int post_id) {
-    return new CommentResponse(true, null);
+    List<Comment> comments = commentDao.getAllComments(post_id);
+    return new CommentResponse(true, null, comments);
   }
 }
--- src/main/java/com/garrettestrin/PrivateGram/data/CommentDao.java
+++ src/main/java/com/garrettestrin/PrivateGram/data/CommentDao.java
@@ -1,8 +1,13 @@
 package com.garrettestrin.PrivateGram.data;
 
+import com.garrettestrin.PrivateGram.data.DataObjects.Comment;
 import org.jdbi.v3.sqlobject.SqlObject;
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import java.util.List;
 
 public interface CommentDao extends SqlObject{
   // TODO: JAVADOC
@@ -10,5 +15,12 @@ public interface CommentDao extends SqlObject{
           + "`comments` (`post_id`, `comment`, `user_id`) "
           +   "VALUES (:post_id, :comment, :user_id) ")
   boolean postComment(@Bind("post_id") int post_id, @Bind("comment") String comment, @Bind("user_id") String user_id);
+
+//  TODO: JAVADOC
+  @SqlQuery("SELECT * "
+            + "FROM `comments` "
+            + "WHERE post_id = :post_id")
+  @RegisterBeanMapper(Comment.class)
+  List<Comment> getAllComments(@Bind("post_id") int post_id);
 }
 

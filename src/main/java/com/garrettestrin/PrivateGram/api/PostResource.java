bed080f6abf5455488db3f234e04026851cf9d5a
Garrett Estrin <garrett.estrin@gmail.com>
2020-03-23 17:20:22 -0700

    Post Resource, Service and Dao added. Add post route added

---
+++ src/main/java/com/garrettestrin/PrivateGram/api/PostResource.java
@@ -0,0 +1,33 @@
+package com.garrettestrin.PrivateGram.api;
+
+import com.codahale.metrics.annotation.Timed;
+import com.garrettestrin.PrivateGram.api.ApiObjects.Post;
+import com.garrettestrin.PrivateGram.api.ApiObjects.PostResponse;
+import com.garrettestrin.PrivateGram.app.Auth.AuthenticatedUser;
+import com.garrettestrin.PrivateGram.biz.PostService;
+
+import javax.ws.rs.CookieParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/post")
+@Produces(MediaType.APPLICATION_JSON)
+public class PostResource {
+
+  private final PostService postService;
+  private final String AUTH_COOKIE = "elsie_gram_auth";
+
+  public PostResource(PostService postService) {
+    this.postService = postService;
+  }
+  // TODO: JAVADOC
+  // TODO: add error handling
+  @POST
+  @Path("/add")
+  @Timed
+  public PostResponse addComment(Post p, @CookieParam(AUTH_COOKIE) AuthenticatedUser authenticatedUser) {
+    return postService.addPost(p.postContent, p.postImageUrl);
+  }
+}
---
+++ src/main/java/com/garrettestrin/PrivateGram/biz/PostService.java
@@ -0,0 +1,23 @@
+package com.garrettestrin.PrivateGram.biz;
+
+import com.garrettestrin.PrivateGram.api.ApiObjects.PostResponse;
+import com.garrettestrin.PrivateGram.data.PostDao;
+
+public class PostService {
+
+  private final PostDao postDao;
+
+  private final String DELETED_POST_SUCCESS = "post was deleted";
+  private final String DELETED_POST_FAIL = "post was not deleted";
+  private final String EDITED_POST_SUCCESS = "post was edited";
+  private final String EDITED_POST_FAIL = "post was not edited";
+
+  public PostService(PostDao postDao) {
+    this.postDao = postDao;
+  }
+
+  public PostResponse addPost(String postContent, String postImageUrl) {
+    boolean wasPostAdded = postDao.addPost(postContent, postImageUrl);
+    return new PostResponse(wasPostAdded, "Posted", null);
+  }
+}

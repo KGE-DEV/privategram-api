9b8118f658891b534705a183bae8d595625471b1
Garrett Estrin <garrett.estrin@gmail.com>
2020-04-18 22:01:19 -0700

    Added get events endpoint

--- src/main/java/com/garrettestrin/PrivateGram/api/EventResource.java
+++ src/main/java/com/garrettestrin/PrivateGram/api/EventResource.java
@@ -2,10 +2,11 @@ package com.garrettestrin.PrivateGram.api;
 
 import com.codahale.metrics.annotation.Timed;
 import com.garrettestrin.PrivateGram.api.ApiObjects.Event;
+import com.garrettestrin.PrivateGram.api.ApiObjects.EventResponse;
+import com.garrettestrin.PrivateGram.app.Auth.AuthenticatedUser;
 import com.garrettestrin.PrivateGram.biz.EventService;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import com.garrettestrin.PrivateGram.biz.UserService;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/event")
@@ -13,14 +14,39 @@ import javax.ws.rs.core.MediaType;
 public class EventResource {
 
   private final EventService eventService;
+  private final UserService userService;
+  private final String AUTH_COOKIE = "api_auth";
 
-  public EventResource(EventService eventService) {
+  public EventResource(EventService eventService, UserService userService) {
     this.eventService = eventService;
+    this.userService = userService;
   }
 
   @POST
   @Timed
   public void trackEvent(Event event) {
     eventService.saveEvent(event);
+  }
+
+  @GET
+  @Path("/by/event")
+  @Timed
+  public EventResponse getEventsByEventType(@QueryParam("event") String event, @CookieParam(AUTH_COOKIE) AuthenticatedUser authenticatedUser) {
+    String role = userService.getUserRole(authenticatedUser.getUserId());
+    if(!role.equals("admin")) {
+      return EventResponse.builder().success(false).build();
+    }
+    return eventService.getEventsByEventType(event);
+  }
+
+  @GET
+  @Path("/by/user")
+  @Timed
+  public EventResponse getEventsByUser(@QueryParam("userId") int userId, @CookieParam(AUTH_COOKIE) AuthenticatedUser authenticatedUser) {
+    String role = userService.getUserRole(authenticatedUser.getUserId());
+    if(!role.equals("admin")) {
+      return EventResponse.builder().success(false).build();
+    }
+    return eventService.getEventsByUser(userId);
   }
 }
